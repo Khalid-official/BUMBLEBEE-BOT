80303c5a1337f1b3b1938e1b94b709a203293d0b
KHALID OFFICIAL <113862545+Khalid-official@users.noreply.github.com>
2024-07-28 07:39:05 +0300

    Add files via upload

---
+++ plugins/tools-element.js
@@ -0,0 +1,40 @@
+import fetch from 'node-fetch';
+
+let elementHandler = async (m, { conn, text }) => {
+  if (!text) throw '*[❗𝐈𝐍𝐅𝐎❗] Please provide an element symbol or name*';
+
+  try {
+    let res = await fetch(`https://api.popcat.xyz/periodic-table?element=${text}`);
+
+    if (!res.ok) {
+      throw new Error(`API request failed with status ${res.status}`);
+    }
+
+    let buffer = await res.arrayBuffer();
+    let json = JSON.parse(Buffer.from(buffer).toString());
+
+    console.log('JSON response:', json);
+
+    let elementInfo = 
+    `*Element Information:*\n
+     • *Name:* ${json.name}\n
+     • *Symbol:* ${json.symbol}\n
+     • *Atomic Number:* ${json.atomic_number}\n
+     • *Atomic Mass:* ${json.atomic_mass}\n
+     • *Period:* ${json.period}\n
+     • *Phase:* ${json.phase}\n
+     • *Discovered By:* ${json.discovered_by}\n
+     • *Summary:* ${json.summary}`;
+
+    conn.sendFile(m.chat, json.image, 'element.jpg', elementInfo, m);
+  } catch (error) {
+    console.error(error);
+    // Handle the error appropriately
+  }
+};
+
+elementHandler.help = ['element'];
+elementHandler.tags = ['tools'];
+elementHandler.command = /^(element|ele)$/i;
+
+export default elementHandler;
