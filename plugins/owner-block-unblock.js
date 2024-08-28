d6722b3bcf1db4244089cb999b3a65f4b5624e8f
KHALID OFFICIAL <113862545+Khalid-official@users.noreply.github.com>
2024-08-28 18:06:48 +0300

    Update owner-block-unblock.js

--- plugins/owner-block-unblock.js
+++ plugins/owner-block-unblock.js
@@ -1,24 +1,32 @@
-let handler = async (m, { text, conn, usedPrefix, command }) => {
-    let why = `*ERROR, EXAMPLE:*\n*—◉ ${usedPrefix + command} @${m.sender.split("@")[0]}*`
-    let who = m.mentionedJid[0] ? m.mentionedJid[0] : m.quoted ? m.quoted.sender : text ? text.replace(/[^0-9]/g, '') + '@s.whatsapp.net' : false
-    if (!who) conn.reply(m.chat, why, m, { mentions: [m.sender] })
-    let res = [];
-    switch (command) {
-      case "block":
-      case "unblock":
-        if (who) await conn.updateBlockStatus(who, "block").then(() => { res.push(who); })
-        else conn.reply(m.chat, why, m, { mentions: [m.sender] })
-        break;
-      case "unblock":
-      case "unblock":
-        if (who) await conn.updateBlockStatus(who, "unblock").then(() => { res.push(who); })
-        else conn.reply(m.chat, why, m, { mentions: [m.sender] })
-        break;
-    }
-    if (res[0]) conn.reply(m.chat, `https://github.com/Khalid-official *[❗𝐈𝐍𝐅𝐎❗] SUCCESS! USER ${command} ACTION PERFORMED ON ${res ? `${res.map(v => '@' + v.split("@")[0])}` : ''}*`, m, { mentions: res })
+
+const handler = async (m, {text, conn, usedPrefix, command}) => {
+  const datas = global
+  const idioma = datas.db.data.users[m.sender].language
+  const _translate = JSON.parse(fs.readFileSync(`./language/en.json`))
+  const tradutor = _translate.plugins.owner_block_unblock
+
+  const why = `${tradutor.texto1} ${usedPrefix + command} @${m.sender.split('@')[0]}*`;
+  const who = m.mentionedJid[0] ? m.mentionedJid[0] : m.quoted ? m.quoted.sender : text ? text.replace(/[^0-9]/g, '') + '@s.whatsapp.net' : false;
+  if (!who) conn.reply(m.chat, why, m, {mentions: [m.sender]});
+  const res = [];
+  switch (command) {
+    case 'blok': case 'block':
+      if (who) {
+        await conn.updateBlockStatus(who, 'block').then(() => {
+          res.push(who);
+        });
+      } else conn.reply(m.chat, why, m, {mentions: [m.sender]});
+      break;
+    case 'unblok': case 'unblock':
+      if (who) {
+        await conn.updateBlockStatus(who, 'unblock').then(() => {
+          res.push(who);
+        });
+      } else conn.reply(m.chat, why, m, {mentions: [m.sender]});
+      break;
   }
-  
-  handler.command = /^(block|unblock)$/i
-  handler.rowner = true
-  export default handler
-  
+  if (res[0]) conn.reply(m.chat, `${tradutor.texto2[0]} ${command} ${tradutor.texto2[1]} ${res ? `${res.map((v) => '@' + v.split('@')[0])}` : ''}*`, m, {mentions: res});
+};
+handler.command = /^(block|unblock)$/i;
+handler.rowner = true;
+export default handler;
