2d5e616383f5e9c3b794289bc5e1dbfae386200a
KHALID OFFICIAL <113862545+Khalid-official@users.noreply.github.com>
2024-09-10 20:45:04 +0300

    Update Internet-lyrics.js

--- plugins/Internet-lyrics.js
+++ plugins/Internet-lyrics.js
@@ -1,18 +1,20 @@
 import fg from 'api-dylux';
-const handler = async (m, {conn, text}) => {
+
+const handler = async (m, { conn, text }) => {
   const teks = text ? text : m.quoted && m.quoted.text ? m.quoted.text : '';
   if (!teks) throw `*[❗𝐈𝐍𝐅𝐎❗] Enter the name and the artist of the song*`;
   try {
-    const res = await fg.lyrics(text);
+    const res = await fg.lyrics(teks);
     const mes = `▢ *${res.title}*
-▢ *${res.artist}*
+🐝 *${res.artist}*
 
-▢ ${res.lyrics}`;
-    conn.sendFile(m.chat, res.thumb, 'img.png', mes, m);
+🐝 ${res.lyrics}`;
+    conn.reply(m.chat, mes, m);
   } catch (e) {
-    m.react(error);
+    m.react('❗'); // Replace 'error' with a suitable reaction or message
   }
 };
+
 handler.help = ['lyrics'];
 handler.tags = ['tools'];
 handler.command = ['testo', 'lyrics', 'letras'];
