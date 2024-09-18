a130734618f7a66bb15688a167ea78232c803f35
KHALID OFFICIAL <113862545+Khalid-official@users.noreply.github.com>
2024-09-18 13:46:13 +0300

    ★𝐁𝐔𝐌𝐁𝐋𝐄𝐁𝐄𝐄🐝𝐁𝐎𝐓★

--- plugins/sticker-wm.js
+++ plugins/sticker-wm.js
@@ -1,30 +1,23 @@
-import {addExif} from '../src/libraries/sticker.js';
-
-const handler = async (m, {conn, text}) => {
-  const datas = global
-  const idioma = datas.db.data.users[m.sender].language
-  const _translate = JSON.parse(fs.readFileSync(`./src/languages/en.json`))
-  const tradutor = _translate.plugins.sticker_wm
-
-  if (!m.quoted) throw tradutor.texto1;
-  let stiker = false;
-  try {
-    let [packname, ...author] = text.split('|');
-    author = (author || []).join('|');
-    const mime = m.quoted.mimetype || '';
-    if (!/webp/.test(mime)) throw tradutor.texto2;
-    const img = await m.quoted.download();
-    if (!img) throw tradutor.texto3;
-    stiker = await addExif(img, packname || global.packname, author || global.author);
-  } catch (e) {
-    console.error(e);
-    if (Buffer.isBuffer(e)) stiker = e;
-  } finally {
-    if (stiker) conn.sendFile(m.chat, stiker, 'wm.webp', '', m, false, {asSticker: true});
-    else throw tradutor.texto3;
-  }
-};
-handler.help = ['wm <packname>|<author>'];
-handler.tags = ['sticker'];
-handler.command = /^take|robar|wm$/i;
-export default handler;
+import { addExif } from '../lib/sticker.js'
+let handler = async (m, { conn, text }) => {
+if (!m.quoted) throw 'https://github.com/Khalid-official *[❗𝐈𝐍𝐅𝐎❗]  RESPOND TO THE STICKER YOU WANT TO ADD A PACKAGE AND A NAME*'
+let stiker = false
+try {
+let [packname, ...author] = text.split('|')
+author = (author || []).join('|')
+let mime = m.quoted.mimetype || ''
+if (!/webp/.test(mime)) throw 'https://github.com/Khalid-official *[❗𝐈𝐍𝐅𝐎❗] REPLY TO THE STICKER WHO WANTS TO ADD A PACKAGE AND A NAME*'
+let img = await m.quoted.download()
+if (!img) throw 'https://github.com/Khalid-official *[❗𝐈𝐍𝐅𝐎❗]  RESPOND TO THE STICKER YOU WANT TO ADD A PACKAGE AND A NAME*'
+stiker = await addExif(img, packname || global.packname, author || global.author)
+} catch (e) {
+console.error(e)
+if (Buffer.isBuffer(e)) stiker = e
+} finally {
+if (stiker) conn.sendFile(m.chat, stiker, 'wm.webp', '', m, false, { asSticker: true })
+else throw 'https://github.com/Khalid-official *[❗𝐈𝐍𝐅𝐎❗] SORRY, SOMETHING WRONG. CHECK THAT YOU HAVE RESPONDED TO A STICKER AND ADDED A PACKAGE NAME AND A USER NAME*'
+}}
+handler.help = ['wm <packname>|<author>']
+handler.tags = ['sticker']
+handler.command = /^take|steal|wm$/i
+export default handler
