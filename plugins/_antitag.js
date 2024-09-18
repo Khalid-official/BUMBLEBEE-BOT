5ef69e06e39702e0a0e8264b59717537beda5796
KHALID OFFICIAL <113862545+Khalid-official@users.noreply.github.com>
2024-09-18 12:50:28 +0300

    ★𝐁𝐔𝐌𝐁𝐋𝐄𝐁𝐄𝐄🐝𝐁𝐎𝐓★

--- plugins/_antitag.js
+++ plugins/_antitag.js
@@ -1,15 +1,7 @@
 const handler = (m) => m;
 
 handler.before = async function(m) {
-  const MAX_TAGS = 5; // Maximum number of tags allowed
-
-  if (m.mentionedJid.length > MAX_TAGS) {
-    // Delete the message
-    await conn.deleteMessage(m.chat, m.key);
-
-    // Kick the sender
-    await conn.groupParticipantsUpdate(m.chat, [m.sender], 'remove');
-  }
+  if (m.mentionedJid.length >= 20) await conn.groupParticipantsUpdate(m.chat, [m.sender], 'remove');
 };
 
 export default handler;
