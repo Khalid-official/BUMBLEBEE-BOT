b382c70aaf72adeafa4352f3841fa9265b899956
KHALID OFFICIAL <113862545+Khalid-official@users.noreply.github.com>
2024-10-07 21:16:56 +0300

    ★𝐁𝐔𝐌𝐁𝐋𝐄𝐁𝐄𝐄🐝𝐁𝐎𝐓★

--- plugins/gc-add.js
+++ plugins/gc-add.js
@@ -1,44 +1,27 @@
+var handler = async (m, { conn, args, text, usedPrefix, command }) => {
 
-const {generateWAMessageFromContent, prepareWAMessageMedia, proto} = (await import("baileys")).default;
-import fetch from 'node-fetch';
-const {getBinaryNodeChild, getBinaryNodeChildren} = (await import("baileys")).default;
-const handler = async (m, {conn, text, participants, args}) => {
-  const idioma = global.db.data.users[m.sender].language || global.defaultLenguaje 
-  const _translate = JSON.parse(fs.readFileSync(`./src/languages/en.json`))
-  const tradutor = _translate.plugins.gc_add
+let who 
+if (m.isGroup) who = m.mentionedJid[0] ? m.mentionedJid[0] : m.quoted ? m.quoted.sender : text
+else who = m.chat
+let name = await conn.getName(m.sender)        
+let user = global.db.data.users[who]
+let nom = conn.getName(m.sender)
+if (!global.db.data.settings[conn.user.jid].restrict) return conn.reply(m.chat, `🚩 *This command is disabled by my creator*`, m, rcanal) 
+if (!text) await m.reply(`🍟 *Please enter the number of the person you want to add to this group*.\n\n🚩 Example:\n*${usedPrefix + command}* 66666666666`)
+if (text.includes('+')) await m.reply(`🍟 Please enter the number all together without the *(+)*`)
+let group = m.chat
+let link = 'https://chat.whatsapp.com/' + await conn.groupInviteCode(group)
 
-  if (!global.db.data.settings[conn.user.jid].restrict) throw tradutor.texto1;
-  if (!args[0]) throw tradutor.texto2;
-  try {
-    const _participants = participants.map((user) => user.id);
-    const users = (await Promise.all(
-        text.split(',')
-            .map((v) => v.replace(/[^0-9]/g, ''))
-            .filter((v) => v.length > 4 && v.length < 20 && !_participants.includes(v + '@s.whatsapp.net'))
-            .map(async (v) => [v, await conn.onWhatsApp(v + '@s.whatsapp.net')]))).filter((v) => v[1][0]?.exists).map((v) => v[0] + '@c.us');
-    const response = await conn.query({tag: 'iq', attrs: {type: 'set', xmlns: 'w:g2', to: m.chat}, content: users.map((jid) => ({tag: 'add', attrs: {}, content: [{tag: 'participant', attrs: {jid}}]}))});
-    const pp = await conn.profilePictureUrl(m.chat).catch((_) => null);
-    const jpegThumbnail = pp ? await (await fetch(pp)).buffer() : Buffer.alloc(0);
-    const add = getBinaryNodeChild(response, 'add');
-    const participant = getBinaryNodeChildren(add, 'participant');
-    for (const user of participant.filter((item) => item.attrs.error == 403)) {
-      const jid = user.attrs.jid;
-      const content = getBinaryNodeChild(user, 'add_request');
-      const invite_code = content.attrs.code;
-      const invite_code_exp = content.attrs.expiration;
-      const teks = `${tradutor.texto3[0]} @${jid.split('@')[0]}, ${tradutor.texto3[1]}`;
-      m.reply(teks, null, {mentions: conn.parseMention(teks)});
-      const captionn = tradutor.texto4;
-      const messaa = await prepareWAMessageMedia({image: jpegThumbnail}, {upload: conn.waUploadToServer});
-      const groupInvite = generateWAMessageFromContent(m.chat, proto.Message.fromObject({groupInviteMessage: {groupJid: m.chat, inviteCode: invite_code, inviteExpiration: invite_code_exp, groupName: await conn.getName(m.chat), caption: captionn, jpegThumbnail: jpegThumbnail}}), {userJid: jid});
-      await conn.relayMessage(jid, groupInvite.message, {messageId: groupInvite.key.id});
-    }
-  } catch {
-    throw tradutor.texto5;
-  }
-};
-handler.help = ['add', '+'].map((v) => v + ' número');
-handler.tags = ['group'];
-handler.command = /^(add|agregar|añadir|\+)$/i;
-handler.admin = handler.group = handler.botAdmin = true;
-export default handler;
+await conn.reply(text+'@s.whatsapp.net', `*🍟 Hello! I am Bumblebee-bot, Someone has invited you to their group.*\n\n*Link*\n${link}`, m, {mentions: [m.sender]})
+await m.reply(`🍟 *Sending the invitation to the private chat of ${nom}*\n\n*📅 ${fecha}*\n⏰ *${tiempo}*`) 
+
+}
+handler.help = ['add']
+handler.tags = ['group']
+handler.command = ['add', 'agregar', 'añadir']
+handler.group = true
+handler.admin = true
+handler.botAdmin = true
+handler.fail = null
+
+export default handler
