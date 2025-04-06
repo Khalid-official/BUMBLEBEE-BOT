89c8b983c157a69104d21473fdca27a1312b8420
KHALID OFFICIAL <113862545+Khalid-official@users.noreply.github.com>
2025-04-06 16:10:50 +0300

    ★𝐁𝐔𝐌𝐁𝐋𝐄𝐁𝐄𝐄🐝𝐁𝐎𝐓★

--- plugins/downloader-play.js
+++ plugins/downloader-play.js
@@ -9,7 +9,7 @@ const { ytmp3, ytmp4 } = require("@hiudyy/ytdl");
 let handler = async (m, { conn, args, text, usedPrefix, command }) => {    
 const datas = global;
 const idioma = datas.db.data.users[m.sender].language || global.defaultLenguaje;
-const _translate = JSON.parse(fs.readFileSync(`./src/languages/${idioma}.json`));
+const _translate = JSON.parse(fs.readFileSync(`./src/languages/en.json`));
 const tradutor = _translate.plugins.descargas_play;
 if (!text) throw `${tradutor.texto1[0]} ${usedPrefix + command} ${tradutor.texto1[1]}`;
       
