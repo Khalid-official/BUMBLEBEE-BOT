484ac6b1ead8cec5b609cd0b5773d1ce4d902009
KHALID OFFICIAL <113862545+Khalid-official@users.noreply.github.com>
2025-04-10 21:44:03 +0300

    ★𝐁𝐔𝐌𝐁𝐋𝐄𝐁𝐄𝐄🐝𝐁𝐎𝐓★

--- plugins/downloader-play.js
+++ plugins/downloader-play.js
@@ -29,9 +29,7 @@ conn.sendMessage(m.chat, { image: { url: yt_play[0].thumbnail }, caption: texto1
 if (command === 'play') {
 try {
 const audiodlp = await ytmp3(yt_play[0].url);
-conn.sendMessage(m.chat, {
-  document: { url: res.result.download },
-  mimetype: 'audio/mpeg',
+conn.sendMessage(m.chat, { document: audiodlp, mimetype: 'audio/mpeg',
   fileName: `${yt_play[0].title}.mp3`,
   caption: `_★𝐁𝐔𝐌𝐁𝐋𝐄𝐁𝐄𝐄🐝𝐁𝐎𝐓★_`
 }, { quoted: m });
@@ -42,9 +40,7 @@ const validQualities = ['64', '96', '128', '192', '256', '320'];
 const selectedQuality = validQualities.includes(quality) ? quality : '320';
 const res = await ogmp3.download(yt_play[0].url, selectedQuality, 'audio');
 
-await conn.sendMessage(m.chat, {
-  document: { url: res.result.download },
-  mimetype: 'audio/mpeg',
+await conn.sendMessage(m.chat, { document: { url: res.result.download }, mimetype: 'audio/mpeg',
   fileName: `${yt_play[0].title}.mp3`,
   caption: `_★𝐁𝐔𝐌𝐁𝐋𝐄𝐁𝐄𝐄🐝𝐁𝐎𝐓★_`
 }, { quoted: m });
@@ -53,9 +49,7 @@ try {
 const res = await fetch(`https://api.siputzx.my.id/api/d/ytmp3?url=${yt_play[0].url}`);
 let { data } = await res.json();
 
-await conn.sendMessage(m.chat, {
-  document: { url: res.result.download },
-  mimetype: 'audio/mpeg',
+await conn.sendMessage(m.chat, { document: { url: data.dl }, mimetype: 'audio/mpeg',
   fileName: `${yt_play[0].title}.mp3`,
   caption: `_★𝐁𝐔𝐌𝐁𝐋𝐄𝐁𝐄𝐄🐝𝐁𝐎𝐓★_`
 }, { quoted: m });
@@ -64,9 +58,7 @@ try {
 const res = await fetch(`https://api.agatz.xyz/api/ytmp3?url=${yt_play[0].url}`)
 let data = await res.json();
 
-await conn.sendMessage(m.chat, {
-  document: { url: res.result.download },
-  mimetype: 'audio/mpeg',
+await conn.sendMessage(m.chat, { document: { url: data.data.downloadUrl }, mimetype: 'audio/mpeg',
   fileName: `${yt_play[0].title}.mp3`,
   caption: `_★𝐁𝐔𝐌𝐁𝐋𝐄𝐁𝐄𝐄🐝𝐁𝐎𝐓★_`
 }, { quoted: m });
@@ -74,7 +66,8 @@ await conn.sendMessage(m.chat, {
 try {
       const apidownload = await axios.get(`https://skynex.boxmine.xyz/docs/download/ytmp3?url=https://youtube.com/watch?v=${yt_play[0].videoId}&apikey=GataDios`)
       const responsev2 = await apidownload.data.data.download;
-         
+     
+                
       await conn.sendMessage(m.chat, { document: { url: responsev2 }, mimetype: 'audio/mpeg',
   fileName: `${yt_play[0].title}.mp3`,
   caption: `_★𝐁𝐔𝐌𝐁𝐋𝐄𝐁𝐄𝐄🐝𝐁𝐎𝐓★_`
@@ -87,22 +80,22 @@ try {
     if (command === 'play') {
         try {
 const video = await ytmp4(yt_play[0].url);
-await conn.sendMessage(m.chat, { video: { url: video }, fileName: `video.mp4`, mimetype: 'video/mp4', caption: `${yt_play[0].title}`}, { quoted: m })
+await conn.sendMessage(m.chat, { document: { url: video }, fileName: `${yt_play[0].title}.mp4`, mimetype: 'video/mp4', caption: `${yt_play[0].title}`}, { quoted: m })
 } catch {
 try {   
 const res = await fetch(`https://api.siputzx.my.id/api/d/ytmp4?url=${yt_play[0].url}`);
 let { data } = await res.json();
-await conn.sendMessage(m.chat, { video: { url: data.dl }, fileName: `video.mp4`, mimetype: 'video/mp4', caption: `${yt_play[0].title}`}, { quoted: m })
+await conn.sendMessage(m.chat, { document: { url: data.dl }, fileName: `${yt_play[0].title}.mp4`, mimetype: 'video/mp4', caption: `${yt_play[0].title}`}, { quoted: m })
 } catch {
 try {  
 const res = await fetch(`https://api.agatz.xyz/api/ytmp4?url=${yt_play[0].url}`)
 let data = await res.json();
-await conn.sendMessage(m.chat, { video: { url: data.data.downloadUrl }, fileName: `video.mp4`, caption: `${yt_play[0].title}` }, { quoted: m }) 
+await conn.sendMessage(m.chat, { document: { url: data.data.downloadUrl }, fileName: `${yt_play[0].title}.mp4`, caption: `${yt_play[0].title}` }, { quoted: m }) 
 } catch {
 try {
 const res = await fetch(`https://api.zenkey.my.id/api/download/ytmp4?apikey=zenkey&url=${yt_play[0].url}`)
 let { result } = await res.json()
-await conn.sendMessage(m.chat, { video: { url: result.download.url }, fileName: `video.mp4`, caption: `${yt_play[0].title}` }, { quoted: m }) 
+await conn.sendMessage(m.chat, { document: { url: result.download.url }, fileName: `${yt_play[0].title}.mp4`, caption: `${yt_play[0].title}` }, { quoted: m }) 
 } catch {
 try {
 const axeelApi = `https://axeel.my.id/api/download/video?url=${yt_play[0].url}`;
@@ -110,12 +103,12 @@ const axeelRes = await fetch(axeelApi);
 const axeelJson = await axeelRes.json();
 if (axeelJson && axeelJson.downloads?.url) {
 const videoUrl = axeelJson.downloads.url;
-await conn.sendMessage(m.chat, { video: { url: videoUrl }, fileName: `${yt_play[0].title}.mp4`, caption: `${yt_play[0].title}` }, { quoted: m }) 
+await conn.sendMessage(m.chat, { document: { url: videoUrl }, fileName: `${yt_play[0].title}.mp4`, caption: `${yt_play[0].title}` }, { quoted: m }) 
 }} catch {
 try {              
 const apidownload = await axios.get(`https://skynex.boxmine.xyz/docs/download/ytmp4?url=https://youtube.com/watch?v=${yt_play[0].videoId}&apikey=GataDios`)
  const responsev2 = await apidownload.data.data.download;         
-   await conn.sendMessage(m.chat, { video: { url: responsev2 }, mimetype: 'video/mp4' }, { quoted: m });
+   await conn.sendMessage(m.chat, { document: { url: responsev2 }, mimetype: 'video/mp4' }, { quoted: m });
    } catch (e) {
     conn.reply(m.chat, `*[ ❌️ ] An error occurred while processing your request.*\n\n${e}`, m);
    }
