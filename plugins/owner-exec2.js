37280cd32679c5131d26470e2d488683eb47d03b
KHALID OFFICIAL <113862545+Khalid-official@users.noreply.github.com>
2025-06-09 00:32:18 +0300

    ★𝐁𝐔𝐌𝐁𝐋𝐄𝐁𝐄𝐄🐝𝐁𝐎𝐓★

--- plugins/owner-exec2.js
+++ plugins/owner-exec2.js
@@ -1,20 +1,26 @@
-import cp, { exec as _exec } from 'child_process'
-import { promisify } from 'util'
-let exec = promisify(_exec).bind(cp)
-let handler = async (m, { conn, isOwner, command, text, usedPrefix, args, isROwner }) => {
-if (!isROwner) return  
-if (global.conn.user.jid != conn.user.jid) return
-m.reply('https://github.com/Khalid-official *[❗𝐈𝐍𝐅𝐎❗]  Executing order...*')
-let o
-try {
-o = await exec(command.trimStart()  + ' ' + text.trimEnd())
-} catch (e) {
-o = e
-} finally {
-let { stdout, stderr } = o
-if (stdout.trim()) m.reply(stdout)
-if (stderr.trim()) m.reply(stderr)
-}}
-handler.customPrefix = /^[$]/
-handler.command = new RegExp
-export default handler
+import cp, {exec as _exec} from 'child_process';
+import {promisify} from 'util';
+
+const exec = promisify(_exec).bind(cp);
+const handler = async (m, {conn, isOwner, command, text, usedPrefix, args, isROwner}) => {
+  if (!isROwner) return;
+  const datas = global
+  const idioma = datas.db.data.users[m.sender].language || global.defaultLenguaje
+  const _translate = JSON.parse(fs.readFileSync(`./src/languages/en.json`))
+  const tradutor = _translate.plugins.owner_exec2
+
+  m.reply(tradutor.texto1);
+  let o;
+  try {
+    o = await exec(command.trimStart() + ' ' + text.trimEnd());
+  } catch (e) {
+    o = e;
+  } finally {
+    const {stdout, stderr} = o;
+    if (stdout.trim()) m.reply(stdout);
+    if (stderr.trim()) m.reply(stderr);
+  }
+};
+handler.customPrefix = /^[~$]/;
+handler.command = new RegExp;
+export default handler;
