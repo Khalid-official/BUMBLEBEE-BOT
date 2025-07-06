379bb241cb9b4cf5e3ff0c1807c81d3025293cb2
KHALID OFFICIAL <113862545+Khalid-official@users.noreply.github.com>
2025-07-06 04:23:40 +0300

    ★𝐁𝐔𝐌𝐁𝐋𝐄𝐁𝐄𝐄🐝𝐁𝐎𝐓★

--- plugins/Internet-lyrics.js
+++ plugins/Internet-lyrics.js
@@ -1,4 +1,4 @@
-import { getTracks } from "@green-code/music-track-data";
+/*import { getTracks } from "@green-code/music-track-data";
 import { googleImage } from "@bochilteam/scraper";
 import got from "got";
 import cheerio from "cheerio";
