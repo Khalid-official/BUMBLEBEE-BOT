7796725690cb4df1c37661426764c281f36f66ef
KHALID OFFICIAL <113862545+Khalid-official@users.noreply.github.com>
2025-09-11 20:29:35 +0300

    Delete plugins directory

--- plugins/owner-leavegc.js
+++
@@ -1,8 +0,0 @@
-let handler = async (m, { conn, text, command }) => {
-let id = text ? text : m.chat  
-await conn.reply(id, 'https://github.com/Khalid-official *[❗𝐈𝐍𝐅𝐎❗] Goodbye everyone, the Bot says goodbye! (≧ω≦)ゞ*')
-await conn.groupLeave(id)}
-handler.command = /^(out|leavegc|leave|salirdelgrupo)$/i
-handler.group = true
-handler.rowner = true
-export default handler
